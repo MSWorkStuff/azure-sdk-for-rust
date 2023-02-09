3b5bd1578765acade82bf7d8034f98362bd940b4
Jose Alvarez <jp.alvarezl@gmail.com>
2023-02-08 19:33:39 -0800

    Using Azure.core reqwest

--- sdk/openai/src/client/mod.rs
+++ sdk/openai/src/client/mod.rs
@@ -1,27 +1,30 @@
+use std::sync::Arc;
+
+use azure_core::{HttpClient, Request, Url, Method};
 use super::Result;
 #[cfg(feature="blocking")]
 pub mod blocking;
 
-pub struct OpenAIClient(reqwest::Client);
+pub struct OpenAIClient(Arc<dyn HttpClient>);
 
 impl OpenAIClient {
 
     pub fn new() -> OpenAIClient {
-        let client = reqwest::Client::builder()
-            // .user_agent()
-            .build()
-            .expect("Error building HTTP client");
+        let client = azure_core::new_http_client();
         OpenAIClient(client)
     }
 
     pub async fn authenticate(&self) {}
 
     pub async fn get_completions(&self) -> Result<String> {
-        let completion_text = self.0.get("https://www.rust-lang.org")
-            .send()
+        let request = Request::new(Url::parse("https://www.rust-lang.org")?, Method::Get);
+
+        let completion_text = self.0.execute_request(&request)
             .await?
-            .text()
+            .into_body()
+            .collect()
             .await?;
+        let completion_text = String::from_utf8(completion_text.into())?; // use serde::from_slice
         Ok(completion_text)
     }
 }
